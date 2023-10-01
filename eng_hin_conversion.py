24ad92bf5382a876869fa01ae3ce92c5e44ae73d
DecentMakeover <Ryanloaded@live.com>
2023-10-01 23:36:37 +0530

    Update eng_hin_conversion.py

--- eng_hin_conversion.py
+++ eng_hin_conversion.py
@@ -1,10 +1,6 @@
-
-#pip install transformers
-#pip install huggingface_hub
-#huggingface-cli login 
-
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
+# Load the pre-trained model and tokenizer
 tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-600M", use_auth_token=True, src_lang="eng_Latn")
 model = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-600M", use_auth_token=True)
 
@@ -13,6 +9,7 @@ def return_translation(sentence):
     translated_tokens = model.generate(**inputs, forced_bos_token_id=tokenizer.lang_code_to_id["hin_Deva"], max_length=30)
     return tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
 
+# Example sentences for translation
 
 sentence_one = "So even if it's a big video, I will clearly mention all the products."
 #model output = तो भले ही यह एक बड़ा वीडियो हो, मैं स्पष्ट रूप से सभी उत्पादों का उल्लेख करूंगा।
@@ -23,5 +20,6 @@ sentence_two ="Definitely share your feedback in the comment section."
 sentence_three = "I was waiting for my bag."
 #model_output = मैं अपने बैग के लिए इंतजार कर रहा था.
 
+# Translate a sentence
 result = return_translation(sentence_three)
 print(result)
